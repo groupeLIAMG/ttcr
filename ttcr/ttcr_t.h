c8352630d495b713e6328e27b844cf856366566e
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2016-09-16 08:17:32 -0400

    allow compiling with MS Visual Studio

--- ttcr/ttcr_t.h
+++ ttcr/ttcr_t.h
@@ -578,7 +578,7 @@ namespace ttcr {
         return v.x*v.x + v.y*v.y + v.z*v.z;
     }
     
-    
+#ifndef _MSC_VER
     // following 3 fct from
     // http://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
     template <typename T> inline constexpr
@@ -595,7 +595,22 @@ namespace ttcr {
     int signum(T x) {
         return signum(x, std::is_signed<T>());
     }
+#else
+    template <typename T>
+    int signum(T x, std::false_type is_signed) {
+        return T(0) < x;
+    }
     
+    template <typename T>
+    int signum(T x, std::true_type is_signed) {
+        return (T(0) < x) - (x < T(0));
+    }
+    
+    template <typename T>
+    int signum(T x) {
+        return signum(x, std::is_signed<T>());
+    }
+#endif
 }
 
 #endif
