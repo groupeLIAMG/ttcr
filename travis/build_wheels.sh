56ed7fe4b639449ed0f6c4de2522948d4fdb8b82
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2020-03-31 14:05:10 -0400

    added script to build wheels

---
+++ travis/build_wheels.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -x
+
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install ttcrpy --no-index -f /io/wheelhouse
+#    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+done
