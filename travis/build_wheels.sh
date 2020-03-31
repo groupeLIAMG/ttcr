57ef40c89142158b40af75282ca83416ba1e7691
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2020-03-31 14:39:30 -0400

    fix

--- travis/build_wheels.sh
+++ travis/build_wheels.sh
@@ -2,13 +2,13 @@
 set -e -x
 
 
+[ ! -d "/io/wheelhouse" ] && mkdir -p "/io/wheelhouse"
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
-
-[ ! -d /io/wheelhouse ] && mkdir -p /io/wheelhouse
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
