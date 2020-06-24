3551a038a67346745f9513741d0f742411c5ed8b
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2020-06-23 20:35:08 -0400

    get rid of now useless hack

--- ttcrpy/Grid2Dttcr.cpp
+++ ttcrpy/Grid2Dttcr.cpp
@@ -29,8 +29,6 @@
 
 using namespace std;
 
-#define import_array_throw() {if (_import_array() < 0) {PyErr_Print(); PyErr_SetString(PyExc_ImportError, "numpy.core.multiarray failed to import"); throw NUMPY_IMPORT_ARRAY_RETVAL; } }
-
 namespace ttcr {
 
     Grid2Dttcr::Grid2Dttcr(std::string& _type,
@@ -203,7 +201,6 @@ namespace ttcr {
         }
 
         // rays
-        import_array_throw();  // to use PyArray_SimpleNewFromData
 
         for ( size_t nv=0; nv<vTx.size(); ++nv ) {
             for ( size_t ni=0; ni<iTx[nv].size(); ++ni ) {
@@ -422,13 +419,13 @@ namespace ttcr {
             }
         }
     }
-    
+
     void Grid2Dttcr::raytrace(const std::vector<sxz<double>>& Tx,
                               const std::vector<double>& tTx,
                               const std::vector<sxz<double>>& Rx,
                               double* traveltimes,
                               PyObject* L) const {
-        
+
         /*
          Looking for redundants Tx pts
          */
@@ -442,7 +439,7 @@ namespace ttcr {
         iTx.push_back( vector<size_t>(1, 0) );  // indices of Rx corresponding to current Tx
         for ( size_t ntx=1; ntx<nTx; ++ntx ) {
             bool found = false;
-            
+
             for ( size_t nv=0; nv<vTx.size(); ++nv ) {
                 if ( vTx[nv][0]==Tx[ntx] ) {
                     found = true;
@@ -460,20 +457,20 @@ namespace ttcr {
         /*
          Looping over all non redundant Tx
          */
-        
+
         vector<sxz<double>> vRx;
         vector<vector<double>> tt( vTx.size() );
         vector<vector<siv2<double>>> L_data(nTx);
         vector<vector<vector<siv2<double>>>> l_data( vTx.size() );
-        
+
         if ( grid_instance->getNthreads() == 1 || vTx.size() <= grid_instance->getNthreads() ) {
             for ( size_t nv=0; nv<vTx.size(); ++nv ) {
-                
+
                 vRx.resize( 0 );
                 for ( size_t ni=0; ni<iTx[nv].size(); ++ni ) {
                     vRx.push_back( Rx[ iTx[nv][ni] ] );
                 }
-                
+
                 try {
                     grid_instance->raytrace(vTx[nv], t0[nv], vRx, tt[nv], l_data[nv]);
                 } catch (std::exception& e) {
@@ -484,18 +481,18 @@ namespace ttcr {
             size_t num_threads = grid_instance->getNthreads() < vTx.size() ? grid_instance->getNthreads() : vTx.size();
             size_t blk_size = vTx.size()/num_threads;
             if ( blk_size == 0 ) blk_size++;
-            
+
             vector<thread> threads(num_threads-1);
             size_t blk_start = 0;
             for ( size_t i=0; i<num_threads-1; ++i ) {
-                
+
                 size_t blk_end = blk_start + blk_size;
                 grid *grid_ref = grid_instance;
                 threads[i]=thread( [&grid_ref,&vTx,&tt,&t0,&Rx,&iTx,
                                     &l_data,blk_start,blk_end,i]{
-                    
+
                     for ( size_t nv=blk_start; nv<blk_end; ++nv ) {
-                        
+
                         vector<sxz<double>> vRx;
                         for ( size_t ni=0; ni<iTx[nv].size(); ++ni ) {
                             vRx.push_back( Rx[ iTx[nv][ni] ] );
@@ -507,10 +504,10 @@ namespace ttcr {
                         }
                     }
                 });
-                
+
                 blk_start = blk_end;
             }
-            
+
             for ( size_t nv=blk_start; nv<vTx.size(); ++nv ) {
                 vector<sxz<double>> vRx;
                 for ( size_t ni=0; ni<iTx[nv].size(); ++ni ) {
@@ -522,50 +519,49 @@ namespace ttcr {
                     throw;
                 }
             }
-            
+
             std::for_each(threads.begin(),threads.end(),
                           std::mem_fn(&std::thread::join));
         }
-        
+
         for ( size_t nv=0; nv<vTx.size(); ++nv ) {
             for ( size_t ni=0; ni<iTx[nv].size(); ++ni ) {
                 traveltimes[ iTx[nv][ni] ] = tt[nv][ni];
             }
         }
-        import_array_throw();  // to use PyArray_SimpleNewFromData
 
         // L
         // first element of tuple contains data, size is nnz
         // second element contains column indices for rows of the matrix, size is nnz
         // third element contains pointers for indices, size is nrow+1 (nTx+1)
-        
+
         for ( size_t nv=0; nv<vTx.size(); ++nv ) {
             for ( size_t ni=0; ni<iTx[nv].size(); ++ni ) {
                 L_data[ iTx[nv][ni] ] = l_data[nv][ni];
             }
         }
-        
+
         size_t nnz = 0;
         for ( size_t n=0; n<L_data.size(); ++n ) {
             nnz += L_data[n].size();
         }
         size_t ncell = grid_instance->getNumberOfCells();
-        
+
         if ( type.compare("iso")==0 ) {
             npy_intp dims[] = {static_cast<npy_intp>(nnz)};
             double* data_p = new double[nnz];
             PyObject* data = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, data_p);
             PyArray_ENABLEFLAGS((PyArrayObject*)data, NPY_ARRAY_OWNDATA);
-            
+
             int64_t* indices_p = new int64_t[nnz];
             PyObject* indices = PyArray_SimpleNewFromData(1, dims, NPY_INT64, indices_p);
             PyArray_ENABLEFLAGS((PyArrayObject*)indices, NPY_ARRAY_OWNDATA);
-            
+
             dims[0] = nTx+1;
             int64_t* indptr_p = new int64_t[nTx+1];
             PyObject* indptr = PyArray_SimpleNewFromData(1, dims, NPY_INT64, indptr_p);
             PyArray_ENABLEFLAGS((PyArrayObject*)indptr, NPY_ARRAY_OWNDATA);
-            
+
             size_t k = 0;
             for ( size_t i=0; i<nTx; ++i ) {
                 indptr_p[i] = k;
@@ -580,28 +576,28 @@ namespace ttcr {
                 }
             }
             indptr_p[nTx] = k;
-            
+
             PyTuple_SetItem(L, 0, data);
             PyTuple_SetItem(L, 1, indices);
             PyTuple_SetItem(L, 2, indptr);
-            
+
         } else {
             nnz *= 2;
-            
+
             npy_intp dims[] = {static_cast<npy_intp>(nnz)};
             double* data_p = new double[nnz];
             PyObject* data = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, data_p);
             PyArray_ENABLEFLAGS((PyArrayObject*)data, NPY_ARRAY_OWNDATA);
-            
+
             int64_t* indices_p = new int64_t[nnz];
             PyObject* indices = PyArray_SimpleNewFromData(1, dims, NPY_INT64, indices_p);
             PyArray_ENABLEFLAGS((PyArrayObject*)indices, NPY_ARRAY_OWNDATA);
-            
+
             dims[0] = nTx+1;
             int64_t* indptr_p = new int64_t[nTx+1];
             PyObject* indptr = PyArray_SimpleNewFromData(1, dims, NPY_INT64, indptr_p);
             PyArray_ENABLEFLAGS((PyArrayObject*)indptr, NPY_ARRAY_OWNDATA);
-            
+
             size_t k = 0;
             for ( size_t i=0; i<nTx; ++i ) {
                 indptr_p[i] = k;
@@ -620,7 +616,7 @@ namespace ttcr {
                 }
             }
             indptr_p[nTx] = k;
-            
+
             PyTuple_SetItem(L, 0, data);
             PyTuple_SetItem(L, 1, indices);
             PyTuple_SetItem(L, 2, indptr);
@@ -823,8 +819,6 @@ namespace ttcr {
 
         data_p = (double*)realloc( data_p, nnz*sizeof(double) );
         indices_p = (int64_t*)realloc( indices_p, nnz*sizeof(int64_t) );
-
-        import_array_throw();  // to use PyArray_SimpleNewFromData
 
         npy_intp dims[] = {static_cast<npy_intp>(nnz)};
         PyObject* data = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, data_p);
@@ -1041,8 +1035,6 @@ namespace ttcr {
 
         data_p = (double*)realloc( data_p, nnz*sizeof(double) );
         indices_p = (int64_t*)realloc( indices_p, nnz*sizeof(int64_t) );
-
-        import_array_throw();  // to use PyArray_SimpleNewFromData
 
         npy_intp dims[] = {static_cast<npy_intp>(nnz)};
         PyObject* data = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, data_p);
