b229667585103c78f17638aedcee7751e930cc02
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2020-09-14 14:14:08 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #
 # python setup.py build_ext --inplace
 
-import glob
 import platform
 import os
 import numpy as np
@@ -16,7 +15,7 @@ if platform.system() == 'Darwin':
     os.environ['CC'] = 'clang'
     os.environ['CXX'] = 'clang++'
     os.environ['MACOSX_DEPLOYMENT_TARGET'] = '10.15'
-    extra_compile_args = ['-std=c++11', '-stdlib=libc++', '-O3'] #, '-DDEBUG_RP']
+    extra_compile_args = ['-std=c++11', '-stdlib=libc++', '-O3']
 elif platform.system() == 'Windows':
     extra_compile_args = ['/O2']
 elif platform.system() == 'Linux':
@@ -86,6 +85,7 @@ setup(
         'Programming Language :: Cython',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering'
         ],
     url='https://github.com/groupeLIAMG/ttcr',
