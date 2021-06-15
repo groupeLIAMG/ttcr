b4ae91b4ecee884ecff2057450c1e31b70d0f8b4
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2021-06-15 15:08:18 -0400

    add python 3.9

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup(
         'Programming Language :: Cython',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering'
         ],
     url='https://github.com/groupeLIAMG/ttcr',
