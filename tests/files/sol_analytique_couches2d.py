43d0ff05f5683ad9bfa90e0a38ba5ba070afe30b
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2023-02-14 07:56:38 -0500

    fix issues with taup

--- tests/files/sol_analytique_couches2d.py
+++ tests/files/sol_analytique_couches2d.py
@@ -18,6 +18,23 @@ z = np.arange(N)
 
 tt = np.zeros((N, N))
 
+a = 1.0
+V20 = 3.0
+b = (V20-a)/20.0
+v = []
+for j in np.arange(N+1):
+    zc = j+0.5
+    v.append(a + b*zc)
+
+with open('couches.tvel', 'w') as f:
+    print('/* Modèle en couche */', file=f)
+    print('/* Solution avec TauP */', file=f)
+    for j in np.arange(N+1):
+        print('{0:f} {1:f} {2:f} {3:f}'.format(j, v[j], 0.6*v[j], 2.67), file=f)
+
+cmd = ['taup_create', '-tvel', 'couches.tvel']
+subprocess.run(cmd, stdout=subprocess.PIPE,
+               env={"PATH": "/usr/local/TauP/bin:/usr/bin/java:/usr/bin"}) 
 for h in z:
     for km in x:
         if h == 0 and km == 0:
@@ -26,11 +43,13 @@ for h in z:
             hh = km * 0.04 / 20.0
         else:
             hh = h
-        cmd = ['taup_time', '-mod', 'couches', '-h', str(hh), '-km', str(km),
+        cmd = ['taup_time', '-mod', 'couches', '-h', '0', '--stadepth', str(hh), '-km', str(km),
                 '-ph', 'p', '--time']
 
         result = subprocess.run(cmd, stdout=subprocess.PIPE,
                                 env={"PATH": "/usr/local/TauP/bin:/usr/bin/java:/usr/bin"})
+        print(cmd)
+        print(h, km, result)
         try:
             tt[km, h] = float(result.stdout)
         except ValueError:
