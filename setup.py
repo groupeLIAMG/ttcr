3e1d4fc4f3aa9a04613d3ebeb365cf62d357189f
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2023-02-15 18:41:05 -0500

    Revert "move to using pyproject.toml"
    This reverts commit 0ced43df6f9bb3860a8af67797ba6790d8ecfb9b.

--- setup.py
+++ setup.py
@@ -5,9 +5,27 @@ import platform
 import os
 import subprocess
 import numpy as np
-from setuptools import Extension, setup
+from setuptools import setup
 from setuptools.command.build_ext import build_ext
+from setuptools.extension import Extension
 from Cython.Build import cythonize
+
+class custom_build_ext(build_ext):
+    def build_extensions(self):
+        # Override the compiler executables. Importantly, this
+        # removes the "default" compiler flags that would
+        # otherwise get passed on to to the compiler, i.e.,
+        # distutils.sysconfig.get_var("CFLAGS").
+        # Done here to remove "-Wsign-compare"
+        if platform.system() == 'Darwin':
+            self.compiler.set_executable("compiler_so", "/usr/bin/clang")
+            self.compiler.set_executable("compiler_cxx", "/usr/bin/clang++")
+            self.compiler.set_executable("linker_so", "/usr/bin/clang++")
+        build_ext.build_extensions(self)
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 if platform.system() == 'Darwin':
     extra_compile_args = ['-std=c++11', '-stdlib=libc++', '-O3',
@@ -30,8 +48,7 @@ elif platform.system() == 'Linux':
 
 include_dirs = ['ttcr', 'boost_1_81_0', 'eigen-3.4.0', np.get_include()]
 
-setup(
-    ext_modules=cythonize([
+extensions = [
     Extension('ttcrpy.rgrid',
               sources=['ttcrpy/rgrid.pyx', 'ttcrpy/verbose.cpp'],  # additional source file(s)
               include_dirs=include_dirs,
@@ -46,5 +63,35 @@ setup(
               extra_compile_args=extra_compile_args,
               extra_link_args=extra_link_args,
               ),
-    ], language_level=3)
+]
+
+setup(
+    name='ttcrpy',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='Code to perform raytracing for geophysical applications',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Bernard Giroux',
+    author_email='bernard.giroux@ete.inrs.ca',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: C++',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering'
+        ],
+    url='https://github.com/groupeLIAMG/ttcr',
+    ext_modules=cythonize(extensions, language_level=3),
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["docs/*.md"],
+        },
+    cmdclass={"build_ext": custom_build_ext}
 )
